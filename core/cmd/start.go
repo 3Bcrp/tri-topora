8050f9a56215e3cd00f157b16da827fd236b5edd
roch1990 <roch1990@tochka.com>
2018-02-25 12:46:04 +0500

    Second commit

--- core/cmd/start.go
+++ core/cmd/start.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -10,16 +8,21 @@ import (
 var RootCmd = &cobra.Command{
 	Use:   "nicedice",
 	Short: "Tri-topora core",
+	Long: "Too start please define config file",
+	RunE: start,
 }
 
 var cfgFile string
 
 func init() {
-	cobra.OnInitialize(initConfig)
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "c", "path to config file")
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "Path to config file")
 	viper.BindPFlag("config", RootCmd.PersistentFlags().Lookup("config"))
 }
 
-func initConfig() {
-	fmt.Println("It's config!")
+func Execute() {
+	RootCmd.Execute()
+}
+
+func start(cmd *cobra.Command, args []string) error {
+    return nil
 }
